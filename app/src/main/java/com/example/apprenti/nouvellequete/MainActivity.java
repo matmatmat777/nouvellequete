7c3c5075bf7742b8b005f9bca09e1edc9ccb7b73
Mathieu <matdelmas@gmail.com>
2017-02-24 17:47:07 +0100

    derniere version de la quete un false apparait

--- app/src/main/java/com/example/apprenti/nouvellequete/MainActivity.java
+++ app/src/main/java/com/example/apprenti/nouvellequete/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.apprenti.nouvellequete;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import static com.example.apprenti.nouvellequete.R.id.boutonV;
+
+public class MainActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final CheckBox checkBox = (CheckBox) findViewById(R.id.checkBox);
+        final EditText nom = (EditText) findViewById(R.id.nom);
+        final EditText prenom = (EditText) findViewById(R.id.prenom);
+
+        nom.setEnabled(false);
+        prenom.setEnabled(false);
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
+
+                nom.setEnabled(checkBox.isChecked());
+                prenom.setEnabled(checkBox.isChecked());
+                boutonV.setEnabled(checkBox.isChecked());
+            }
+        });
+
     }
+
+
+
+
 }
